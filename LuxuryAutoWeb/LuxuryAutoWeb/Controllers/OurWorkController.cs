a7e10a35ce090c3a8a76d059fded02fc33dc0fa8
cmelo72 <chris@cloudnetworkz.com>
2012-12-16 19:18:40 -0500

    Initial commit

---
+++ LuxuryAutoWeb/LuxuryAutoWeb/Controllers/OurWorkController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LuxuryAutoWeb.Controllers
+{
+    public class OurWorkController : Controller
+    {
+        //
+        // GET: /OurWork/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
